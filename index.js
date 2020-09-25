6171dbf8affc674e51836c213a22612da08fe179
admin <admin@sgg.com>
2020-09-25 16:24:49 +0800

    log access

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var {router}=require('./luckyshopee');
 app.use(express.static(path.join(__dirname, 'app/dist'), {maxAge:7*24*3600*1000, index: 'index.html' }));
 if (argv.debugout) {
     app.use(function(req, res, next) {
-        debugout('access', req.url, req.headers);
+        debugout('access', req.url, {...req.query, ...req.body});
         next();
     });
 }
