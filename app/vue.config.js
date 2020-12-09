9f3c7f9dad5e0dbdb123fd0e6b7ca37a375a2710
admin <admin@sgg.com>
2020-12-09 12:08:12 +0800

    test s

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,12 +1,24 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports={
     pages:{
         index:'src/main.js',
         admin:'src/admin/main.js',
         setting: 'src/setting/main.js',
-        sharepoint:{
-            entry:'src/sharepoint/main.js',
-            filename:'s',
-        }
+        // sharepoint:{
+        //     entry:'src/sharepoint/main.js',
+        //     filename:'s',
+        // }
     },
     productionSourceMap: false,
+    configureWebpack :{
+        plugins:[
+            new CopyPlugin({
+                patterns: [
+                  { from: "src/sharepoint/s.html", to: "s" , toType:'file'},
+                ],
+            }),
+        ]
+    
+    }
 }
